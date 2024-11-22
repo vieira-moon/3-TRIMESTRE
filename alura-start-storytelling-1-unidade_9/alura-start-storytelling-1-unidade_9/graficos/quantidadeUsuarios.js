84e16d96a92cced305ac0d32056613d3c1f8d4de
Amelia Vieira <113919867+vieira-moon@users.noreply.github.com>
2024-11-22 09:20:04 -0300

    Update quantidadeUsuarios.js
    sfgdsgdfg

--- alura-start-storytelling-1-unidade_9/alura-start-storytelling-1-unidade_9/graficos/quantidadeUsuarios.js
+++ alura-start-storytelling-1-unidade_9/alura-start-storytelling-1-unidade_9/graficos/quantidadeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./common.js"
+import { getCSS, tickConfig } from "./common.js"
 
 async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
@@ -20,7 +20,34 @@ async function quantidadeUsuariosPorRede() {
 
     const layout = {
         plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color')
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Redes sociais com mais usários no mundo',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title:{
+                text: 'nome das redes sociais',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title:{
+                text: 'bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
 
     }
 
