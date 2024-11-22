416ca2d826408d2dc2628f7316bcbdd0207323b9
Amelia Vieira <113919867+vieira-moon@users.noreply.github.com>
2024-11-22 09:19:13 -0300

    Update common.js
    gdfgdgd

--- alura-start-storytelling-1-unidade_9/alura-start-storytelling-1-unidade_9/graficos/common.js
+++ alura-start-storytelling-1-unidade_9/alura-start-storytelling-1-unidade_9/graficos/common.js
@@ -2,4 +2,10 @@ const getCSS = (variavel) => {
     return getComputedStyle(document.body).getPropertyValue(variavel)
 }
 
-export {getCSS}
+const tickConfig = {
+    color: getCSS('--primary-color'),
+    size: 16,
+    family: getCSS('--font')
+}
+
+export { getCSS, tickConfig }
